291e5964ed70a702ec1ce9d59faad4bfca9dd6a8
zarico87 <zarinarico@hotmail.com>
2026-05-04 11:04:56 -0300

    subiendo modificaciones

---
+++ script/script.js
@@ -0,0 +1,17 @@
+/*const btnMenu = document.getElementById("btn-menu");
+const menu = document.querySelector(".link-container");
+
+btnMenu.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
+*/
+
+
+/* UX: cerrar menú al hacer click */
+/* document.querySelectorAll(".link-container a").forEach(link => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+});
+*/
+
